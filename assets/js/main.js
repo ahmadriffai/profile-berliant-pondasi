84b5ceb0b5bac283ed3be36b846899dc38f5783c
ahmadriffai <rifai0850@gmail.com>
2022-02-14 18:49:07 +0700

    home and galery

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,19 @@
 $(document).ready(function () { 
+    // navbar shrink
+    $(window).on("scroll", function(){
+        if($(this).scrollTop() > 90){
+            $(".navbar").addClass("navbar-shrink")
+        }else{
+            $(".navbar").removeClass("navbar-shrink")
+        }
+    })
+
     // section2
     $('.section2-carousel').owlCarousel({
         loop:true,
         margin:0,
         responsiveClass:true,
+        autoplay: true,
         responsive:{
             0:{
                 items:1
